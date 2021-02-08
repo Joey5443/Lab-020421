9d3f03435aa59e2d59f2103551fdd3a890b1a60c
Joey5443 <77410060+Joey5443@users.noreply.github.com>
2021-02-08 12:21:26 -0800

    Init commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//allow the user tot enter their guess
+let num = prompt('Guess an number between 1 and 10');
+// check to see if number is a winner
+if (num == 5) {
+    //If ti is, they got it right
+    alert('You guessed the correct number!');
+
+} else {
+    // If it isn't, they got it wrong
+    alert('You guessed the wrong number!');
+}
